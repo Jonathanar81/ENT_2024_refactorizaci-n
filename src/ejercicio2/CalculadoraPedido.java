6f8f8d5ac9cb4388851bdce6514d24a1864b0fa5
Mañana <Maana@Aula8-20>
2024-04-23 08:58:10 +0200

    Inicial

---
+++ src/ejercicio2/CalculadoraPedido.java
@@ -0,0 +1,8 @@
+package ejercicio2;
+
+public class CalculadoraPedido {
+	public double calcularPrecioTotalPedido(Pedido pedido) {
+		return 0;
+        // Lógica para calcular el precio total de un pedido
+    }
+}
